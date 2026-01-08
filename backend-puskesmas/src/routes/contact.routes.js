2cba11cc3de290835f95e2c3c8cdf5d0f5ef632d
IkmalKadafi <thariqkadafi@gmail.com>
2026-01-08 13:25:24 +0700

    Back to Beginning

--- backend-puskesmas/src/routes/contact.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contactController = require('../controllers/contactController');
-
-router.post('/', contactController.sendMessage);
-router.get('/', contactController.getAllMessages);
-
-module.exports = router;
--- backend-puskesmas/src/routes/inovasi.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const innovationController = require('../controllers/innovationController');
-
-router.get('/', innovationController.getAllInnovations);
-router.get('/:slug', innovationController.getInnovationBySlug);
-
-module.exports = router;
